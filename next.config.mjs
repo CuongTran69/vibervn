cbabc89539cd5f3f03d0e63022320a77098f2b9c
Cường Trần <cuongtv50@viettel.com.vn>
2026-02-04 16:21:57 +0700

    update build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  distDir: 'dist',
   trailingSlash: true,
   images: {
     unoptimized: true,
